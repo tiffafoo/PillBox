8a4b83bb7295605bd1866c3184a298a01f82781d
Matan Kushner <matchai@me.com>
2017-01-28 14:31:26 -0500

    Add frontend routing

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.scss';
+import logo from '../logo.svg';
+import '../App.scss';
 
-class App extends React.Component {
+class NotFound extends React.Component {
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Search</h2>
+          <h2>404</h2>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
@@ -18,4 +18,4 @@ class App extends React.Component {
   }
 }
 
-export default App;
+export default NotFound;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import { render } from 'react-dom';
+import { BrowserRouter, Match, Miss } from 'react-router';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+import './index.scss';
+import App from './App';
+import Splash from './components/Splash';
+import NotFound from './components/NotFound';
+
+const Root = () => {
+  return (
+    <BrowserRouter>
+      <div>
+        <Match exactly pattern="/" component={Splash} />
+        <Match pattern="/search" component={App} />
+        <Miss component={NotFound} />
+      </div>
+    </BrowserRouter>
+  )
+}
+
+render(<Root/>, document.getElementById('root'));
